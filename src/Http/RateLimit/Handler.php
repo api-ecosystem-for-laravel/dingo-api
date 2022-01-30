d79c23119629601c6d77817d46895ade380a9052
Max <contact@maxsnow.me>
2022-01-31 00:12:15 +1100

    Fix sorting for php 8

--- src/Http/RateLimit/Handler.php
+++ src/Http/RateLimit/Handler.php
@@ -105,7 +105,7 @@ class Handler
         // limiting will not be imposed for the request.
         } else {
             $this->throttle = $this->getMatchingThrottles()->sort(function ($a, $b) {
-                return $a->getLimit() < $b->getLimit();
+                return $a->getLimit() <=> $b->getLimit();
             })->first();
         }
 
