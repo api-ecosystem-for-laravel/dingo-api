26b2b3d932c55488ab4c90f537126a1891bd77c4
Robert Deutz <rdeutz@googlemail.com>
2024-06-18 12:46:12 +0200

    move affecting type hints
    Moving the type hint at the end of the function.

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -125,12 +125,12 @@ class Response extends IlluminateResponse
      */
     public function morph($format = 'json')
     {
-        $this->content = $this->getOriginalContent() ?? '';
+        $content = $this->getOriginalContent() ?? '';
 
         $this->fireMorphingEvent();
 
-        if (isset(static::$transformer) && static::$transformer->transformableResponse($this->content)) {
-            $this->content = static::$transformer->transform($this->content);
+        if (isset(static::$transformer) && static::$transformer->transformableResponse($content)) {
+            $content = static::$transformer->transform($content);
         }
 
         $formatter = static::getFormatter($format);
@@ -147,19 +147,21 @@ class Response extends IlluminateResponse
 
         $this->fireMorphedEvent();
 
-        if ($this->content instanceof EloquentModel) {
-            $this->content = $formatter->formatEloquentModel($this->content);
-        } elseif ($this->content instanceof EloquentCollection) {
-            $this->content = $formatter->formatEloquentCollection($this->content);
-        } elseif (is_array($this->content) || $this->content instanceof ArrayObject || $this->content instanceof Arrayable) {
-            $this->content = $formatter->formatArray($this->content);
-        } elseif ($this->content instanceof stdClass) {
-            $this->content = $formatter->formatArray((array) $this->content);
+        if ($content instanceof EloquentModel) {
+            $content = $formatter->formatEloquentModel($content);
+        } elseif ($content instanceof EloquentCollection) {
+            $content = $formatter->formatEloquentCollection($content);
+        } elseif (is_array($content) || $content instanceof ArrayObject || $content instanceof Arrayable) {
+            $content = $formatter->formatArray($content);
+        } elseif ($content instanceof stdClass) {
+            $content = $formatter->formatArray((array) $content);
         } else {
             if (! empty($defaultContentType)) {
                 $this->headers->set('Content-Type', $defaultContentType);
             }
         }
+
+        $this->content = $content;
 
         return $this;
     }
